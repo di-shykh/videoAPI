ca42f025ac0b95c24963bb3ba64f60633e378411
di-shykh <di49@mail.ru>
2025-11-11 12:44:02 +0300

    feat: add some validation

--- src/videos/validation/videoInputToValidation.ts
+++ src/videos/validation/videoInputToValidation.ts
@@ -1,4 +1,33 @@
 import {ValidationError} from "../types/validationsErrors";
-import {Video} from "../types/video";
+import {VideoInput} from "../dto/video.input";
+import {VideoCreateInput} from "../dto/video-create.input";
+import {Resolutions} from "../types/video";
 
-export
+export const videoInputToValidation = (
+    data: VideoInput
+): ValidationError[] =>{
+    const errors: ValidationError[] = [];
+    console.log('validation data:', data);
+    const {
+        title,
+        author,
+        canBeDownloaded,
+        minAgeRestriction,
+        createdAt,
+        publicationDate,
+        availableResolutions,
+    } = data.attributes;
+    if(!title || typeof title !=='string'|| title.trim().length<2||title.trim().length>40){
+        errors.push({field: 'title', message: 'Invalid title'});
+    }
+    if(!author || typeof author !=='string'||  author.trim().length<2||author.trim().length>20){
+        errors.push({field: 'author', message: 'Invalid author'});
+    }
+    if(canBeDownloaded === undefined || typeof canBeDownloaded !=='boolean'){
+        errors.push({field: 'canBeDownloaded', message: 'Invalid canBeDownloaded'});
+    }
+    if(!author || typeof author !=='string'||  author.trim().length<2||author.trim().length>15){
+        errors.push({field: 'author', message: 'Invalid author'});
+    }
+}
+
